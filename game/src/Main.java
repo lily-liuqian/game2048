7fe3e188ce49498c4217c6d175636bd4f0c1b7a6
lily-liuqian <ab060829@outlook.com>
2025-05-07 20:41:40 +0800

    hot-fix-01

--- game/src/Main.java
+++ game/src/Main.java
@@ -5,5 +5,6 @@ public class Main {
             System.out.println("Hello World");
             System.out.println("Hello World1");
             System.out.println("Hello World2");
+            System.out.println("Hello World3");
         }
     }
